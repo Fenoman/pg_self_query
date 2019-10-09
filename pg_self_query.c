b78ec7fd9377c1bd305b15ceb79bc829bb43570e
fenoman <isupervisor@gmail.com>
2019-10-10 00:57:14 +0300

    test

--- pg_self_query.c
+++ pg_self_query.c
@@ -90,7 +90,6 @@ typedef struct
 	char	stack[FLEXIBLE_ARRAY_MEMBER];	/* sequencially laid out stack frames in form of text records */
 } stack_msg;
 
-static List *GetCurrentQueryStates();
 
 /*
  * Module load callback
@@ -235,6 +234,131 @@ typedef struct
 {
 	const char	*query;
 } stack_frame_analyze;
+
+
+static stack_msg *
+copy_msg(stack_msg *msg)
+{
+	stack_msg *result = palloc(msg->length);
+
+	memcpy(result, msg, msg->length);
+	return result;
+}
+
+/*		
+  *	Get List of stack_frames as a stack of function calls starting from outermost call.		
+  *		Each entry contains only query text.		
+  *	Assume extension is enabled and QueryDescStack is not empty		
+  */		
+ static List *		
+ runtime_explain()		
+ {		
+ 	ListCell	    *i;		
+ 	List			*result = NIL;		
+
+  	Assert(list_length(QueryDescStack) > 0);		
+
+  	/* collect query state outputs of each plan entry of stack */		
+ 	foreach(i, QueryDescStack)		
+ 	{		
+ 		QueryDesc 	*currentQueryDesc = (QueryDesc *) lfirst(i);		
+ 		stack_frame_analyze	*qs_frame = palloc(sizeof(stack_frame_analyze));		
+
+  		/* save query text */		
+ 		qs_frame->query = currentQueryDesc->sourceText;		
+
+  		result = lcons(qs_frame, result);		
+ 	}		
+
+  	return result;		
+ }
+
+ /*
+ * Compute length of serialized stack frame
+ */
+static int
+serialized_stack_frame_length(stack_frame_analyze *qs_frame)
+{
+	return 	INTALIGN(strlen(qs_frame->query) + VARHDRSZ);
+}
+
+/*
+ * Compute overall length of serialized stack of function calls
+ */
+static int
+serialized_stack_length(List *qs_stack)
+{
+	ListCell 	*i;
+	int			result = 0;
+
+	foreach(i, qs_stack)
+	{
+		stack_frame_analyze *qs_frame = (stack_frame_analyze *) lfirst(i);
+		result += serialized_stack_frame_length(qs_frame);
+	}
+
+	return result;
+}
+
+/*
+ * Convert stack_frame_analyze record into serialized text format version
+ * 		Increment '*dest' pointer to the next serialized stack frame
+ */
+static void
+serialize_stack_frame(char **dest, stack_frame_analyze *qs_frame)
+{
+	SET_VARSIZE(*dest, strlen(qs_frame->query) + VARHDRSZ);
+	memcpy(VARDATA(*dest), qs_frame->query, strlen(qs_frame->query));
+	*dest += INTALIGN(VARSIZE(*dest));
+}
+
+/*
+ * Convert List of stack_frame_analyze records into serialized structures laid out sequentially
+ */
+static void
+serialize_stack(char *dest, List *qs_stack)
+{
+	ListCell		*i;
+	foreach(i, qs_stack)
+	{
+		stack_frame_analyze *qs_frame = (stack_frame_analyze *) lfirst(i);
+		serialize_stack_frame(&dest, qs_frame);
+	}
+}
+
+/*
+ * Get state of current query.
+ */
+static stack_msg *
+GetQueryState()
+{
+	List			*qs_stack = runtime_explain();
+	int				msglen = sizeof(stack_msg) + serialized_stack_length(qs_stack);
+	stack_msg		*msg = palloc(msglen);
+
+	msg->length = msglen;
+	msg->proc = MyProc;
+	msg->result_code = QS_RETURNED;
+	msg->warnings = 0;	
+	msg->stack_depth = list_length(qs_stack);
+	serialize_stack(msg->stack, qs_stack);
+
+	return msg;
+}
+
+static List *
+GetCurrentQueryStates()
+{
+	List			*result = NIL;
+	stack_msg		*msg;
+
+	msg = GetQueryState();
+	result = lappend(result, copy_msg(msg));
+	
+	return result;
+}
+
+
 
 /*
  *	Convert serialized stack frame into stack_frame record
@@ -432,126 +556,4 @@ pg_self_query(PG_FUNCTION_ARGS)
 	{
 		SRF_RETURN_DONE(funcctx);
 	}
-}
-
-static stack_msg *
-copy_msg(stack_msg *msg)
-{
-	stack_msg *result = palloc(msg->length);
-
-	memcpy(result, msg, msg->length);
-	return result;
-}
-
-/*		
-  *	Get List of stack_frames as a stack of function calls starting from outermost call.		
-  *		Each entry contains only query text.		
-  *	Assume extension is enabled and QueryDescStack is not empty		
-  */		
- static List *		
- runtime_explain()		
- {		
- 	ListCell	    *i;		
- 	List			*result = NIL;		
-
-  	Assert(list_length(QueryDescStack) > 0);		
-
-  	/* collect query state outputs of each plan entry of stack */		
- 	foreach(i, QueryDescStack)		
- 	{		
- 		QueryDesc 	*currentQueryDesc = (QueryDesc *) lfirst(i);		
- 		stack_frame_analyze	*qs_frame = palloc(sizeof(stack_frame_analyze));		
-
-  		/* save query text */		
- 		qs_frame->query = currentQueryDesc->sourceText;		
-
-  		result = lcons(qs_frame, result);		
- 	}		
-
-  	return result;		
- }
-
- /*
- * Compute length of serialized stack frame
- */
-static int
-serialized_stack_frame_length(stack_frame_analyze *qs_frame)
-{
-	return 	INTALIGN(strlen(qs_frame->query) + VARHDRSZ);
-}
-
-/*
- * Compute overall length of serialized stack of function calls
- */
-static int
-serialized_stack_length(List *qs_stack)
-{
-	ListCell 	*i;
-	int			result = 0;
-
-	foreach(i, qs_stack)
-	{
-		stack_frame_analyze *qs_frame = (stack_frame_analyze *) lfirst(i);
-		result += serialized_stack_frame_length(qs_frame);
-	}
-
-	return result;
-}
-
-/*
- * Convert stack_frame_analyze record into serialized text format version
- * 		Increment '*dest' pointer to the next serialized stack frame
- */
-static void
-serialize_stack_frame(char **dest, stack_frame_analyze *qs_frame)
-{
-	SET_VARSIZE(*dest, strlen(qs_frame->query) + VARHDRSZ);
-	memcpy(VARDATA(*dest), qs_frame->query, strlen(qs_frame->query));
-	*dest += INTALIGN(VARSIZE(*dest));
-}
-
-/*
- * Convert List of stack_frame_analyze records into serialized structures laid out sequentially
- */
-static void
-serialize_stack(char *dest, List *qs_stack)
-{
-	ListCell		*i;
-	foreach(i, qs_stack)
-	{
-		stack_frame_analyze *qs_frame = (stack_frame_analyze *) lfirst(i);
-		serialize_stack_frame(&dest, qs_frame);
-	}
-}
-
-/*
- * Get state of current query.
- */
-static stack_msg *
-GetQueryState()
-{
-	List			*qs_stack = runtime_explain();
-	int				msglen = sizeof(stack_msg) + serialized_stack_length(qs_stack);
-	stack_msg		*msg = palloc(msglen);
-
-	msg->length = msglen;
-	msg->proc = MyProc;
-	msg->result_code = QS_RETURNED;
-	msg->warnings = 0;	
-	msg->stack_depth = list_length(qs_stack);
-	serialize_stack(msg->stack, qs_stack);
-
-	return msg;
-}
-
-static List *
-GetCurrentQueryStates()
-{
-	List			*result = NIL;
-	stack_msg		*msg;
-
-	msg = GetQueryState();
-	result = lappend(result, copy_msg(msg));
-	
-	return result;
 }
